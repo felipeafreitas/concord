bd6bdfc9858feca943ef5dc120ce693ceadee283
Felipe Freitas <afelipe.freitas@gmail.com>
2021-12-05 13:18:05 -0300

    addint register endpoint

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,17 +1,22 @@
-import { Input, Button, Box, Wrap, Spacer, InputGroup, InputLeftElement, Text } from '@chakra-ui/react'
+import { Input, Button, Box, Wrap, Spacer, InputGroup, InputLeftElement, Text, FormErrorMessage, FormControl } from '@chakra-ui/react'
 
 import { EmailIcon, LockIcon } from '@chakra-ui/icons'
 
 import AuthenticationCard from '../components/AuthenticationCard'
-import { useState } from 'react';
 import { Field, Form, Formik } from 'formik';
+import api from '../api';
 
 function Register() {
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('')
+const submit = (values: any) => {
+  try {
+    const response = api.post('/register', {
+      body: values
+    })
 
-const submit = (values) => {
+    console.log(response)
+  } catch(err) {
+    console.log(err)
+  }
   console.log(values)
 }
 
@@ -22,30 +27,74 @@ const submit = (values) => {
       <Text fontSize="16px">Master web development by making real-life projects. There are multiple paths for you to choose</Text>
       </Box>
       <Spacer />
-      <Formik onSubmit={(values) => {
-        submit(values)
-      }}
-      initialValues={{ 
-        name: "Name",
-        email: "Email",
-        password: "Password"
-      }}>
+      <Formik 
+        onSubmit={async (values, actions) => {
+          await submit(values);
+          actions.setSubmitting(false)
+        }}
+        initialValues={{ 
+          name: "",
+          email: "",
+          password: ""
+        }}
+        validate={values => {
+          const errors = {} as any
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          } else if (values.password.length < 6) {
+            errors.password = 'Password must have at least 6 length'
+          }
+          return errors;
+        }}
+        >
         {(props) => (
         <Form>
-          <Wrap spacing="14px" marginBottom="22px" display="flex">
+          <Wrap spacing="14px" marginBottom="22px" display="flex" flexDirection="column">
             <Field name="name">
-              <Input placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+              {({ field, form }: any) => (
+                <FormControl isInvalid={form.errors.name && form.touched.name} display="flex" flexDirection="column">
+                  <InputGroup flexDirection="column">
+                    <Input {...field} id="name" placeholder="name"/>
+                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                  </InputGroup>
+                </FormControl>
+              )}
             </Field>
-            <InputGroup>
-              <InputLeftElement children={<EmailIcon />} />
-              <Input placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
-            </InputGroup>
-            <InputGroup>
-              <InputLeftElement children={<LockIcon />} />
-              <Input placeholder='Password' type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            </InputGroup>
+            <Field name="email">
+              {({ field, form }: any) => (
+                <FormControl isInvalid={form.errors.email && form.touched.email}>
+                  <InputGroup flexDirection="column">
+                    <InputLeftElement children={<EmailIcon />} />
+                    <Input {...field} id="email" placeholder="Email"/>
+                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                  </InputGroup>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="password">
+              {({ field, form }: any) => (
+                <FormControl isInvalid={form.errors.password && form.touched.password}>
+                    <InputGroup flexDirection="column">
+                      <InputLeftElement children={<LockIcon />} />
+                      <Input Input {...field} id="password" placeholder="Password"type="password"/>
+                      <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                    </InputGroup>
+                </FormControl>
+              )}
+            </Field>
           </Wrap>
-          <Button colorScheme='blue' width="100%" marginBottom="31px" type='submit'>Start chatting now</Button>
+          <Button 
+            colorScheme='blue' 
+            width="100%" 
+            marginBottom="31px" 
+            type='submit'
+            isLoading={props.isSubmitting}
+          >
+            Start chatting now
+          </Button>
         </Form>
         )}
       </Formik>
